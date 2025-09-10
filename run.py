835b22271fb153b0e27cba127c81219fd4cbcb29
liyijiaaa <2979038380@qq.com>
2025-09-10 20:56:24 +0800

    更新节点嵌入

--- run.py
+++ run.py
@@ -334,7 +334,7 @@ def train_global(global_net, opt, graph, args):
         loss, scores = global_net(feats, epoch, ada_neighbor_nodes)
 
         # 修改在这里添加mix_score
-        beta = math.pow(0.9,epoch)
+        beta = math.pow(0.7,epoch)
         beta2 = 1-beta
         mix_score = -(beta2 * scores + beta * pos)
         if epoch >= warm_up_epoch and (epoch - update_day) >= update_internal:
