6dbe66753300f273f335842181e60b276e1add2f
liyijiaaa <2979038380@qq.com>
2025-10-12 17:53:29 +0800

    更新节点嵌入

--- run.py
+++ run.py
@@ -341,7 +341,7 @@ def train_global(global_net, opt, graph, args):
         if epoch >= warm_up_epoch and (epoch - update_day) >= update_internal:
             # 计算奖励
             r = get_reward(device, p, ppr_adj, hop1_adj, hop2_adj, knn_adj, num_nodes,
-                           ada_neighbor_nodes, cost_mat=pos)
+                           ada_neighbor_nodes, cost_mat=mix_score)
 
             # 基于奖励更新采样权重_两个0.01是可变参数
             updated_param = np.exp((p_min / 2.0) * (r + 0.01 / p) * 100 * np.sqrt(
