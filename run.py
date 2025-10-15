71a30936cb75ec7b366c13bcc6111b3c47e737af
liyijiaaa <2979038380@qq.com>
2025-10-15 09:37:35 +0800

    更新节点嵌入

--- run.py
+++ run.py
@@ -89,7 +89,7 @@ def train_local(net, graph, feats, opt, args, memorybank_nor, memorybank_abnor, 
             train_ano_score_nonzero = torch.count_nonzero(train_ano_score, dim=0)
             train_ano_score = torch.sum(train_ano_score, dim=0)
             train_ano_score = train_ano_score / train_ano_score_nonzero
-            _, train_list = train_ano_score.topk(int(0.30 * num_nodes), dim=0, largest=False, sorted=True)
+            _, train_list = train_ano_score.topk(int(0.50 * num_nodes), dim=0, largest=False, sorted=True)
 
             train_list = train_list.cpu().numpy()
             train_list = train_list.tolist()
@@ -103,7 +103,7 @@ def train_local(net, graph, feats, opt, args, memorybank_nor, memorybank_abnor, 
             abnormal_non_zero_count = torch.count_nonzero(train_ano_scoreclone, dim=0)
             train_ano_scoreclone = torch.sum(train_ano_scoreclone, dim=0)
             train_ano_scoreclone = train_ano_scoreclone / abnormal_non_zero_count
-            _, abnormal_indices = train_ano_scoreclone.topk(int(0.05 * num_nodes), dim=0, largest=True, sorted=True)
+            _, abnormal_indices = train_ano_scoreclone.topk(int(0.01 * num_nodes), dim=0, largest=True, sorted=True)
             abnor_idx = abnormal_indices.cpu().numpy().tolist()
 
         if epoch >= 3:
