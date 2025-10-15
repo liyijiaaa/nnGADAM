05124538322d105da036fe43fdf4b7cb2801be63
liyijiaaa <2979038380@qq.com>
2025-10-15 09:41:57 +0800

    更新节点嵌入

--- run.py
+++ run.py
@@ -89,7 +89,7 @@ def train_local(net, graph, feats, opt, args, memorybank_nor, memorybank_abnor, 
             train_ano_score_nonzero = torch.count_nonzero(train_ano_score, dim=0)
             train_ano_score = torch.sum(train_ano_score, dim=0)
             train_ano_score = train_ano_score / train_ano_score_nonzero
-            _, train_list = train_ano_score.topk(int(0.50 * num_nodes), dim=0, largest=False, sorted=True)
+            _, train_list = train_ano_score.topk(int(0.80 * num_nodes), dim=0, largest=False, sorted=True)
 
             train_list = train_list.cpu().numpy()
             train_list = train_list.tolist()
@@ -103,7 +103,7 @@ def train_local(net, graph, feats, opt, args, memorybank_nor, memorybank_abnor, 
             abnormal_non_zero_count = torch.count_nonzero(train_ano_scoreclone, dim=0)
             train_ano_scoreclone = torch.sum(train_ano_scoreclone, dim=0)
             train_ano_scoreclone = train_ano_scoreclone / abnormal_non_zero_count
-            _, abnormal_indices = train_ano_scoreclone.topk(int(0.01 * num_nodes), dim=0, largest=True, sorted=True)
+            _, abnormal_indices = train_ano_scoreclone.topk(int(0.05 * num_nodes), dim=0, largest=True, sorted=True)
             abnor_idx = abnormal_indices.cpu().numpy().tolist()
 
         if epoch >= 3:
@@ -329,7 +329,7 @@ def train_global(global_net, opt, graph, args):
 
         opt.zero_grad()
         #自适应邻居采样修改——自适应采样——k
-        sampled_result = adaptive_sampler(num_nodes, ppr_adj, hop1_adj, hop2_adj, knn_adj, p=p, total_sample_size=25)
+        sampled_result = adaptive_sampler(num_nodes, ppr_adj, hop1_adj, hop2_adj, knn_adj, p=p, total_sample_size=20)
 
         ada_neighbor_nodes = torch.stack(sampled_result).to(device).detach()
 
@@ -345,7 +345,7 @@ def train_global(global_net, opt, graph, args):
 
             # 基于奖励更新采样权重_两个0.01是可变参数
             updated_param = np.exp((p_min / 2.0) * (r + 0.01 / p) * 100 * np.sqrt(
-                np.log(25 / 0.01) / (sampling_ways * update_internal)))
+                np.log(20 / 0.01) / (sampling_ways * update_internal)))
             # updated_param = np.exp(
             #     (p_min / 2.0) * (r + 1 / p) * np.sqrt(np.log(20 / 0.1) / (sampling_ways * update_internal)))
 
