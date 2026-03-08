00dc30a6c3ac674f5a02cdedcae1fe700185a097
liyijiaaa <2979038380@qq.com>
2026-03-08 10:09:53 +0800

    更新节点嵌入

--- model.py
+++ model.py
@@ -157,7 +157,7 @@ class GlobalModel(nn.Module):
     def msg_pass(self, h, mean_h, attn):
         # h+attn*mean_h
         #nei = attn * self.neigh_weight
-        nei = 0 #自适应消息传递消融
+        nei = 0.5 #自适应消息传递消融
         h = nei * mean_h + (1 - nei) * h
         return h
 
