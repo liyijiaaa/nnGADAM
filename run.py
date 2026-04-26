3e90e31f0f738eeabe7f9a62b2979e9e16936826
liyijiaaa <2979038380@qq.com>
2026-04-26 13:48:37 +0800

    更新节点嵌入

--- run.py
+++ run.py
@@ -98,7 +98,11 @@ def train_local(net, graph, feats, opt, args, memorybank_nor, memorybank_abnor, 
 
             # 计算每个节点在多个epoch中异常池的平均异常得分
             for idx in range(len(memorybank_abnor)):
-                train_ano_scoreclone[idx, memorybank_abnor[idx]] = 0
+                #train_ano_scoreclone[idx, memorybank_abnor[idx]] = 0
+                row = train_ano_scoreclone[idx]
+                mask = torch.ones_like(row, dtype=torch.bool)
+                mask[memorybank_abnor[idx]] = False
+                row[mask] = 0
             abnormal_non_zero_count = torch.count_nonzero(train_ano_scoreclone, dim=0)
             train_ano_scoreclone = torch.sum(train_ano_scoreclone, dim=0)
             train_ano_scoreclone = train_ano_scoreclone / abnormal_non_zero_count
