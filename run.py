7c9b7f1bea3c0d842ef289363ebc4a814ff8e9ed
liyijiaaa <2979038380@qq.com>
2026-04-26 11:41:44 +0800

    更新节点嵌入

--- run.py
+++ run.py
@@ -59,7 +59,7 @@ def train_local(net, graph, feats, opt, args, memorybank_nor, memorybank_abnor, 
         if epoch > 0:
             # 动态添加正太池
             _, train_list_temp = train_ano_score[epoch - 1].topk(
-                int(num_nodes - (epoch / args.local_epochs) ** 2 * num_nodes), dim=0,
+                int((epoch / args.local_epochs) ** 2 * num_nodes), dim=0,
                 largest=False, sorted=True)
             train_list_temp = train_list_temp.cpu().numpy()
             train_list_temp = train_list_temp.tolist()
@@ -67,7 +67,7 @@ def train_local(net, graph, feats, opt, args, memorybank_nor, memorybank_abnor, 
 
             # 动态添加异常池——数量设置的一样
             _, train_list_atemp = train_ano_score[epoch - 1].topk(
-                int(num_nodes - (epoch / args.local_epochs) ** 2 * num_nodes), dim=0,
+                int((epoch / args.local_epochs) ** 2 * num_nodes), dim=0,
                 largest=True, sorted=True)
             train_list_atemp = train_list_atemp.cpu().numpy()
             train_list_atemp = train_list_atemp.tolist()
