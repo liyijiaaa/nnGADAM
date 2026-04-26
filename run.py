4b6628d4eb74253374285eabc2406f5bfda2f629
liyijiaaa <2979038380@qq.com>
2026-04-26 10:13:45 +0800

    更新节点嵌入

--- run.py
+++ run.py
@@ -102,7 +102,7 @@ def train_local(net, graph, feats, opt, args, memorybank_nor, memorybank_abnor, 
             abnormal_non_zero_count = torch.count_nonzero(train_ano_scoreclone, dim=0)
             train_ano_scoreclone = torch.sum(train_ano_scoreclone, dim=0)
             train_ano_scoreclone = train_ano_scoreclone / abnormal_non_zero_count
-            _, abnormal_indices = train_ano_scoreclone.topk(int(0.005 * num_nodes), dim=0, largest=True, sorted=True)
+            _, abnormal_indices = train_ano_scoreclone.topk(int(0.01 * num_nodes), dim=0, largest=True, sorted=True)
             abnor_idx = abnormal_indices.cpu().numpy().tolist()
 
             # 基于多轮平均异常分数得到伪标签
