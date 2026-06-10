5b94d97a6961fb3b53f748f752d67ae3039a7f59
liyijiaaa <2979038380@qq.com>
2026-06-10 21:08:52 +0800

    更新节点嵌入

--- run.py
+++ run.py
@@ -310,7 +310,7 @@ def train_global(global_net, opt, graph, args):
 
     #p = np.array([0.25, 0.25, 0.25, 0.25]) #固定概率消融
     # warm_up_epoch = 3
-    warm_up_epoch = 3
+    warm_up_epoch = 5
     #奖励函数的计算次数
     update_internal = 5
     update_day = -1
